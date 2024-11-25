25348ee29525b3e2dc36d6ce851bf0435a34a56a
ebrahimjmi <mohd.ebrahim.jmi@gmail.com>
2024-11-25 20:31:51 +0530

    implement cart functionality

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const Cart = () => {
+
+    const cartItems = useSelector(state => state.cart.items);
+    console.log(cartItems)
   return (
     <div>Cart page</div>
   )
